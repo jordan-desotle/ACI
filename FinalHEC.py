9adaccd9dc42c34c34f82eaa1120d2d17b36938c
mjs7934 <79617624+mjs7934@users.noreply.github.com>
2022-08-01 08:52:38 -0400

    Update FinalHEC.py

--- FinalHEC.py
+++ FinalHEC.py
@@ -1,4 +1,5 @@
 
+
 from sympy import *
 from sympy import gcd as sm
 from sympy import simplify as simp
@@ -198,9 +199,32 @@ class HyperEllipticCurve:
         degree = len(coeffs)
         for i in range(0, degree):
             eq = eq + coeffs[i]*x**(degree-(i+1))
-        y1 = eq.subs(x, x1[0])
-        y2 = eq.subs(x, x2[0])
-        new_divisor = [[x1[0], y1], [x2[0], y2]]
+        try: 
+            y1 = eq.subs(x, x1)
+            y2 = eq.subs(x, x2)
+        except:
+            valid = False
+            i = 0
+            while (valid == False):
+                try:
+                    temp_eq = 0
+                    temp_coeffs = all_two_root_divs[-(i+1)][1].coefficients()
+                    degree = len(temp_coeffs)
+                    for i in range(0, degree):
+                        temp_eq = temp_eq + temp_coeffs[i]*x**(degree-(i+1))
+                    print("Equation: " + str(temp_eq))
+                    temp_x1 = all_two_root_divs[-(i+1)][0].roots()[0][0]
+                    temp_x2 = all_two_root_divs[-(i+1)][0].roots()[1][0]
+
+                    y1 = temp_eq.subs(x, temp_x1)
+                    y2 = temp_eq.subs(x, temp_x2)
+                    valid = True
+                except:
+                    print("Didn't Work")
+                    i += 1
+            # y1 = int(eq)
+            # y1 = int(eq)
+        new_divisor = [[x1, y1], [x2, y2]]
         return new_divisor
 
 
@@ -216,12 +240,14 @@ class HyperEllipticCurve:
         e1, e2, c1, c2= symbols("e1 e2 c1 c2")
         need_num = False
         
-        d1 = sm(u1, u2)
+        d1 = sm(u1, u2) #assigns the gcd of u1 and u2 to d1
         # print(d1)
         # print("E1, E2: " + str(solve(Eq(e1 * u1 + e2 * u2, d1), e1, e2)))
-        d = sm(d1, v1 + v2)
+        d = sm(d1, v1 + v2) #assigns the gcd of d1 and v1+v2 to d (note, if the h(x) in your curve is not 0, add h to v1+v2)
 
         x = Symbol('x')
+
+        #pulls the coefficients out of the Divisors for a later step (denoted by an *)
         u1_coeffs = poly(u1, x).all_coeffs()
         u2_coeffs = poly(u2, x).all_coeffs()   
         v1_coeffs = poly(v1, x).all_coeffs()
@@ -240,11 +266,11 @@ class HyperEllipticCurve:
             d1_coeffs[0] = 0
 
         
-        
+        #creates a Galois field for us to use sagemath's cantor functions in with x as a variable name.
         R = sage.all.PolynomialRing(GF(Integer(p_val)), names=('x',)); (x,) = R._first_ngens(1)
 
 
-
+        # * lines 250-261 turn the divisors and curve into an object that sagemath can use.
         u1 = int(u1_coeffs[0])*x**2+int(u1_coeffs[1])*x+int(u1_coeffs[2])
         u2 = int(u2_coeffs[0])*x**2+int(u2_coeffs[1])*x+int(u2_coeffs[2])
         if (len(v1_coeffs) == 1):    
@@ -269,17 +295,26 @@ class HyperEllipticCurve:
 
         two_root_divs = []
         for i in range (0, n):
+            
+            #assigns the divisor that's two away from the iteration number to temp_d2
+            # which is then plugged into cantor's algorithm with D1. 
+            # example: on iteration 3, it will add D1 and D5
             temp_d2 = d_arr[i+2]
             D3_reduced = self.sage_cantor(D1, temp_d2, eq, p_val)
-            if (len(D3_reduced[0].roots()) > 1 and len(two_root_divs) < 2 and type(D3_reduced[1]) != int):
+
+            #if the new D3 has two roots, there aren't already a full pair of two root divisors, and D3's v isn't an integer,
+            # add D3 to the two root divisors list.
+            if (len(D3_reduced[0].roots()) > 1 and len(two_root_divs) < 2):
                 two_root_divs.append(D3_reduced)
                 
             if (i == n-1):
+                #if a full pair of two root divisors was never found, pull the 3rd and 2nd to last from the master list of 
+                #two root divisors
                 if (len(two_root_divs) < 2):
+                
                     for j in range (0, (2 - len(two_root_divs))):
-                        two_root_divs.append(all_two_root_divs[-j])
-                        
-                    print("in function: " + str(two_root_divs))
+                        two_root_divs.append(all_two_root_divs[-(j+1)])
+                    
                 if (len(D3_reduced[0].roots()) < 2):
                     for j in range (0, n):
                         temp_d2 = d_arr[j]
@@ -288,7 +323,7 @@ class HyperEllipticCurve:
                             two_root_divs[1] = D3_reduced
                             break
                 if (len(D3_reduced[0].roots()) < 2):
-                    print("you need me")
+                    
                     if (type(D3_reduced[1]) == int):
                         if(D3_reduced[1] == 0):
                             random_num = D3_reduced[0].coefficients()[1]
@@ -311,12 +346,12 @@ class HyperEllipticCurve:
         
         new_points = []
         
+        
         for i in two_root_divs:
             degree = len(i[1].coefficients())
             v3_coeffs = i[1].coefficients()
-            x1 = i[0].roots()[0]
-            x2 = i[0].roots()[1]
-
+            x1 = i[0].roots()[0][0]
+            x2 = i[0].roots()[1][0]
             new_points.append(self.solve_for_new_points(x1, x2, v3_coeffs))
         if (need_num == True):
             random_num = self.pseudo_random_nums(two_root_divs[1])
@@ -335,10 +370,12 @@ random_nums = []
 FILE = "random_nums.txt"
 x = Symbol("x")
 # p_val = 115792089237316195423570985008687907853269984665640564039457584007908834671663 
-#p_val = 6957596529882152968992225251835887181478451547013  
-p_val = 237512715131811281324243117391942323623 #trial 3
+# p_val = 6957596529882152968992225251835887181478451547013  
+# p_val = 237512715131811281324243117391942323623 #trial 3
 
 # p_val = 20988936657440586486151264256610222593863921 #trial 1 - 99.6% random with n=100
+# p_val = 71223472761329346781570947473
+p_val =  14693679385278593849609206715278070972733319459651094018859396328480215743184089660644531
 
 new_divisors = [[x**2 + 7*x + 10, x + 9], [x**2 + 10, 7*x + 9]]
 n = 100
@@ -346,25 +383,39 @@ random_num = 0
 all_two_root_divs = []
 
 hec = HyperEllipticCurve(p, c1, c2, c3, c4, c5, c6)
-for i in range(0, 200):
-    new_num = hec.cantors_algorithm(new_divisors, n, p_val)[0]
+for i in range(0, 100):
+
+    cantors_results = []
+    # Assigns the random_num returned from cantors_algorithm to new_num
+    cantors_results = hec.cantors_algorithm(new_divisors, n, p_val)
+    new_num = cantors_results[0]
     new_points = []
     
-    new_D1 = hec.cantors_algorithm(new_divisors, n, p_val)[1][0]
-    new_D2 = hec.cantors_algorithm(new_divisors, n, p_val)[1][1]
+    #assigns the points for the new starting divisors to new_D1 and new_D2 respectively
+    new_D1 = cantors_results[1][0]
+    new_D2 = cantors_results[1][1]
     
+    #puts the new divisors into muford representation
     new_divisors = hec.mumford_rep(new_D1, new_D2)
 
+    #In the even that the number is less than 10000000000000000000000000000000000000, 
+    #this does point addition again until its high enough.
     while (new_num < 10000000000000000000000000000000000000):
-        new_num = hec.cantors_algorithm(new_divisors, n, p_val)[0]
+        cantors_results = hec.cantors_algorithm(new_divisors, n, p_val)
+        new_num = cantors_results[0]
         new_points = []
         
-        new_D1 = hec.cantors_algorithm(new_divisors, n, p_val)[1][0]
-        new_D2 = hec.cantors_algorithm(new_divisors, n, p_val)[1][1]
+        new_D1 = cantors_results[1][0]
+        new_D2 = cantors_results[1][1]
         new_divisors = hec.mumford_rep(new_D1, new_D2)
     print(str(i+1) + ": " + str(new_num))
+
+    #Adds the new random number to random_nums
     random_nums.append(new_num)
-    for j in hec.cantors_algorithm(new_divisors, n, p_val)[2]:
+
+    #adds the two root divisors from the last iteration to a master list
+    # in case a future iteration doesn't produce enough
+    for j in cantors_results[2]:
         all_two_root_divs.append(j)
         
 
@@ -372,4 +423,5 @@ file = open(FILE, "w")
 for i in random_nums:
     # file.write(bin(i).replace("0b", "") + "\n")
     file.write(str(i) + "\n")
+
 
